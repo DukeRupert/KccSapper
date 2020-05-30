ec7b3ee7206b88a3644e243d56074730134ba716
DukeRupert <loganwilliamsse@gmail.com>
2020-05-30 14:11:05 -0700

    Changed scrollbar colors & size. Sped up logo animation.

---
+++ src/components/menuCard.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let image='';
+    export let alt='';
+    export let title='';
+    export let ingredients='';
+    export let nutrition='';
+    export let allergens='';
+
+    let phoneNumber = 'tel:' + phone;
+</script>
+
+<a href={link}><img src={image} alt={alt}></a>
+<h1>{title}</h1>
+<a href={link}><p>{@html address}</p></a>
+<a id='phone' href={phoneNumber}><p>{phone}</p></a>
+<p>{hours}</p>
+
+<style>
+    img {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+    }
+
+    h1 {
+        font-weight: normal;
+        letter-spacing: 1px;
+    }
+
+    p {
+		width: 100%;
+		font-size: 17px;
+	}
+</style>
--- src/routes/menu.svelte
+++ src/routes/menu.svelte
@@ -1,5 +1,54 @@
+<script>
+
+</script>
+
 <svelte:head>
 	<title>Menu</title>
 </svelte:head>
 
-<h1>Menu</h1>
+<style>
+	.cards {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+		font-size: 1em;
+		text-align: left;
+	}
+
+	.card {
+		flex: 1;
+		min-width: 300px;
+		max-width: 30%;
+		max-height: 70%;
+		margin: 2%;
+		padding: 20px;
+		box-shadow: -2px -2px 5px darkgray, 2px 2px 5px lightgray;
+		border-radius: 20px;
+
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		align-items: flex-start;
+	}
+	div {
+		width: 100%;
+		height: 100%;
+	}
+
+
+  @media only screen and (max-width: 655px) {
+
+  }
+</style>
+
+<div>
+	
+	<div class='cards'>
+		<div class='card'>
+			
+		</div>
+		<div class='card'>
+				
+		</div>
+	</div>
+</div>
