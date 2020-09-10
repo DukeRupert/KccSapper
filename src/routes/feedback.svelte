2e1d9c51ffb8f97adf277a1c8dc418051e1a5302
DukeRupert <loganwilliamsse@gmail.com>
2020-09-10 06:39:06 -0700

    Added netlify form functionality to feedback page

--- src/routes/feedback.svelte
+++ src/routes/feedback.svelte
@@ -1,118 +1,128 @@
 <script>
-	import { contact } from '../store';
+  import { contact } from "../store";
 </script>
 
-<svelte:head>
-	<title>Contact Us</title>
-</svelte:head>
-
 <style>
-	div {
-		display: flex;
-		flex-direction: column;
-
-		box-sizing: border-box;
-		padding: 4em;
-		padding-top: 0;
-		font-size: 2em;
-		text-align: left;
-		background-color:  white;
-		width: 100%;
-	}
-
-	p {
-		font-size: 17px;
-	}
-
-	form {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-		padding: 0;
-		font-size: 17px;
-		margin-bottom: 1em;
-	}
-
-	input {
-		border-radius: 8px;
-	}
-
-	textarea {
-		border-radius: 8px;
-	}
-
-	button {
+  .container {
     display: flex;
+    flex-direction: column;
+
+    box-sizing: border-box;
+    padding: 4em;
+    padding-top: 0;
+    font-size: 2em;
+    text-align: left;
+    background-color: white;
+    width: 100%;
+    margin-top: 80px;
+  }
+
+  p {
+    font-size: 17px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 0;
+    font-size: 17px;
+    margin-bottom: 1em;
+  }
+
+  input {
+    border-radius: 8px;
+  }
+
+  textarea {
+    border-radius: 8px;
+  }
+
+  button {
+    display: flex;
+    align-self: center;
     align-items: center;
     justify-content: center;
     width: 30vw;
     padding: 5px;
-    padding-right: 2rem;
-    padding-left: 2rem;
+    margin-top: 1rem;
+    padding: 0.5em 1em;
     border-radius: 40px;
-	border-color: black;
-	background-color: #F7E7A6;
-  	}
+    border-color: black;
+    background-color: #f7e7a6;
+    transition: all 0.2s ease-in-out;
+  }
 
-	@media only screen and (max-width: 655px) {
-	
-	div {
-		padding: 1em;
-		
-	}
+  button:hover {
+    transform: scale(1.1);
+  }
 
-	form {
-		margin-top: 10%;
-		margin-bottom: 10%;
-	}
+  @media only screen and (max-width: 655px) {
+    .container {
+      padding: 1em;
+    }
 
-	label {
-		font-size: 14px;
-	}
+    form {
+      margin-top: 10%;
+      margin-bottom: 10%;
+    }
 
-	input {
-		font-size: 14px;
-		border-radius: 8px;
-	}
+    label {
+      font-size: 14px;
+    }
 
-	textarea {
-		font-size: 14px;
-		border-radius: 8px;
-	}
-	
-	h1 {
-		font-size: 24px;
-	}
-	p {
-		font-size: 14px;
-	}
+    input {
+      font-size: 14px;
+      border-radius: 8px;
+    }
 
-	a {
-		font-weight: bold;
-	}
+    textarea {
+      font-size: 14px;
+      border-radius: 8px;
+    }
 
-	button {
+    h1 {
+      font-size: 24px;
+    }
+    p {
+      font-size: 14px;
+    }
+
+    a {
+      font-weight: bold;
+    }
+
+    button {
       padding-right: 1rem;
       padding-left: 1rem;
       border-radius: 30px;
       font-size: 14px;
     }
-
-}
+  }
 </style>
 
-<div>
-	<h1>CONTACT US</h1>
-	<p>
-		We’d love to hear from you! Send us an email to <a href='mailto: info@kagenandco.com'>info@kagenandco.com</a> or fill in your details below.
-	</p>
-	<form action="mailto:loganwilliamsse@gmail.com" method="post" enctype="text/plain">
-		<label>NAME</label>
-		<input type="text" bind:value={$contact.name} />
-		<label>EMAIL</label>
-		<input type="text" bind:value={$contact.email} />
-		<label>MESSAGE</label>
-		<textarea rows='6' type="text" bind:value={$contact.message} />
-	</form>
-	<button onclick='submit'>SUBMIT</button>
+<svelte:head>
+  <title>Contact Us</title>
+</svelte:head>
+<div class="container">
+  <h1>CONTACT US</h1>
+  <p>
+    We’d love to hear from you! Send us an email to <a
+      href="mailto: info@kagenandco.com">info@kagenandco.com</a> or fill in your
+    details below.
+  </p>
+  <form
+    name="contact"
+    method="POST"
+    data-netlify="true"
+    data-netlify-recaptcha="true">
+    <label>NAME</label>
+    <input type="text" name="name" />
+    <label>EMAIL</label>
+    <input type="email" name="email" />
+    <label>MESSAGE</label>
+    <textarea rows="6" type="text" name="message" />
+    <div data-netlify-recaptcha="true" />
+    <button type="submit">SUBMIT</button>
+  </form>
 </div>
