7fe6433f6bee406e343e279f53b2f47b9887ce60
DukeRupert <loganwilliamsse@gmail.com>
2020-09-10 10:59:14 -0700

    Troubleshooting netlify forms.

--- src/routes/feedback.svelte
+++ src/routes/feedback.svelte
@@ -112,6 +112,7 @@
     details below.
   </p>
   <form name="contact" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="contact" />
     <p><label>Your Name: <input type="text" name="name" /></label></p>
     <p><label>Your Email: <input type="email" name="email" /></label></p>
     <p>
