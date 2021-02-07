eedb00faacd7102a19c079432b1ca3ec4da9a8ac
DukeRupert <loganwilliamsse@gmail.com>
2021-02-07 14:05:34 -0800

    Moved crepe data to store. Referenced store values in spokane-menu.

--- src/routes/menu-spokane/index.svelte
+++ src/routes/menu-spokane/index.svelte
@@ -3,171 +3,7 @@
   import MenuItem from "../../components/menuItem.svelte";
   import CoffeeItem from "../../components/coffeeItem.svelte";
 
-  const theLemon = {
-    src: "../images/crepe_theLemon.jpg",
-    alt: "A lemon sugar crepe.",
-    title: "The Lemon",
-    ingredients: ["lemon", "sugar", "eggs", "dairy", "flour"],
-    price: {
-      full: "7.00",
-      mini: "6.00",
-    },
-    // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const thePeanutButter = {
-    src: "../images/crepe_thePeanutButter.jpg",
-    alt: "A peanut butter and banana crepe.",
-    title: "The Peanut Butter ",
-    ingredients: ["peanut butter", "banana", "eggs", "dairy", "flour"],
-    price: {
-      full: "9.00",
-      mini: "7.50",
-    },
-
-    // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const theChocolateRaspberry = {
-    src: "../images/crepe_theChocolate.jpg",
-    alt: "A chocolate raspberry crepe.",
-    title: "The Chocolate",
-    ingredients: ["chocolate", "raspberry", "eggs", "dairy", "flour"],
-    price: {
-      full: "9.00",
-      mini: "7.50",
-    }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const theNutella = {
-    src: "../images/crepe_theNutella.jpg",
-    alt: "A strawberry and nutella crepe.",
-    title: "The Nutella",
-    ingredients: ["Nutella", "strawberry", "eggs", "dairy", "flour"],
-    price: {
-      full: "9.00",
-      mini: "7.50",
-    }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const theEgg = {
-    src: "../images/crepe_theEgg.jpg",
-    alt: "An egg crepe with hollandaise.",
-    title: "The Egg",
-    ingredients: ["provolone", "spinach", "eggs", "dairy", "flour"],
-    price: {
-      full: "11.95",
-      mini: "8.50",
-    }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const theBlackBean = {
-    src: "../images/crepe_theBlackBean.jpg",
-    alt: "A black bean crepe.",
-    title: "The Black Bean",
-    ingredients: [
-      "black bean",
-      "avocado",
-      "tomato",
-      "spinach",
-      "lime",
-      "salsa",
-      "eggs",
-      "dairy",
-      "flour",
-    ],
-    price: {
-      full: "11.95",
-      mini: "8.50",
-    }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const theChicken = {
-    src: "../images/crepe_chicken.jpg",
-    alt: "A chicken crepe.",
-    title: "The Chicken",
-    ingredients: ["chicken", "spinach", "provolone", "eggs", "dairy", "flour"],
-    price: {
-      full: "11.95",
-      mini: "8.50",
-    }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const theMonteCristo = {
-    src: "../images/crepe_monteCristo.jpg",
-    alt: "A Monte Cristo crepe.",
-    title: "The Monte Cristo",
-    ingredients: [
-      "ham",
-      "turkey",
-      "provolone",
-      "cheddar",
-      "raspberry",
-      "eggs",
-      "dairy",
-      "flour",
-    ],
-    price: {
-      full: "11.95",
-      mini: "8.50",
-    }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const theTurkeyBacon = {
-    src: "../images/crepe_turkeyBacon.jpg",
-    alt: "A turkey bacon crepe.",
-    title: "The Turkey Bacon",
-    ingredients: [
-      "turkey",
-      "bacon",
-      "provolone",
-      "tomato",
-      "spinach",
-      "ranch",
-      "eggs",
-      "dairy",
-      "flour",
-    ],
-    price: {
-      full: "11.95",
-      mini: "8.50",
-    }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const theChipotleChicken = {
-    src: "../images/crepe_chickenChipotle.jpg",
-    alt: "A chipotle chicken crepe.",
-    title: "The Chipotle Chicken",
-    ingredients: [
-      "chicken",
-      "cole slaw",
-      "tomato",
-      "chipotle ranch",
-      "eggs",
-      "dairy",
-      "flour",
-    ],
-    price: {
-      full: "11.95",
-      mini: "8.50",
-    }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  };
-
-  const savoryCrepes = [
-    theEgg,
-    theChicken,
-    theChipotleChicken,
-    theTurkeyBacon,
-    theBlackBean,
-    theMonteCristo,
-  ];
-
-  const sweetCrepes = [
-    theNutella,
-    thePeanutButter,
-    theLemon,
-    theChocolateRaspberry,
-  ];
+  import { sweetCrepes, savoryCrepes } from "../../store";
 
   const coffees = [
     {
@@ -246,74 +82,6 @@
       price: "$5.00",
     },
   ];
-
-  // const theBreakfastInBed = {
-  //   src: "../images/crepe_theAllAmerican.jpg",
-  //   alt: "A Breakfast in Bed crepe.",
-  //   title: "The Breakfast in Bed",
-  //   ingredients: [
-  //     "bacon",
-  //     "cheddar",
-  //     "maple sausage",
-  //     "maple syrup",
-  //     "eggs",
-  //     "dairy",
-  //     "flour",
-  //   ],
-  //   price: {
-  //     full: "11.95",
-  //     mini: "8.50",
-  //   }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  // };
-
-  // const theCaprese = {
-  //   src: "../images/crepe_caprese.jpg",
-  //   alt: "A Caprese crepe.",
-  //   title: "The Caprese",
-  //   ingredients: [
-  //     "mozzarella",
-  //     "tomato",
-  //     "arugula",
-  //     "spinach",
-  //     "balsamic",
-  //     "eggs",
-  //     "dairy",
-  //     "flour",
-  //   ],
-  //   price: {
-  //     full: "11.95",
-  //     mini: "8.50",
-  //   }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  // };
-
-  // const theAllAmerican = {
-  //   src: "../images/crepe_theAllAmerican.jpg",
-  //   alt: "An All American crepe.",
-  //   title: "The All American",
-  //   ingredients: ["gravy", "bacon", "cheddar", "eggs", "dairy", "flour"],
-  //   price: {
-  //     full: "11.95",
-  //     mini: "8.50",
-  //   }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  // };
-
-  // const theMixedBerry = {
-  //   src: "../images/crepe_theNutella.jpg",
-  //   alt: "A mixed berry crepe.",
-  //   title: "The Mixed Berry",
-  //   ingredients: [
-  //     "cream cheese",
-  //     "strawberry",
-  //     "blueberries",
-  //     "eggs",
-  //     "dairy",
-  //     "flour",
-  //   ],
-  //   price: {
-  //     full: "9.00",
-  //     mini: "7.50",
-  //   }, // nutrition: ['Sugar 4g', 'Fat 2g']
-  // };
 </script>
 
 <svelte:head>
@@ -323,7 +91,7 @@
 <!-- Sweet Crepes -->
 <div class="wrapper">
   <img class="banner" src="/images/sweet.png" alt="sweet" />
-  {#each sweetCrepes as crepe}
+  {#each $sweetCrepes as crepe}
     <div class="card">
       <MenuCard {...crepe} />
     </div>
@@ -334,7 +102,7 @@
 <a class="anchor" id="savory" />
 <div class="wrapper">
   <img class="banner" src="/images/savory.png" alt="sweet" />
-  {#each savoryCrepes as crepe}
+  {#each $savoryCrepes as crepe}
     <div class="card">
       <MenuCard {...crepe} />
     </div>
