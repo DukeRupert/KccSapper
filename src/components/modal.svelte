0a43dd659146c23c4f8f3432db7ceb89b177c9be
DukeRupert <loganwilliamsse@gmail.com>
2021-02-11 10:43:54 -0800

    Increased z-value on modal to place it above navbar dropdown menu.

--- src/components/modal.svelte
+++ src/components/modal.svelte
@@ -52,7 +52,7 @@
   .modal {
     display: none; /* Hidden by default */
     position: fixed; /* Stay in place */
-    z-index: 101; /* Sit on top */
+    z-index: 105; /* Sit on top */
     left: 0;
     top: 0;
     width: 100%; /* Full width */
